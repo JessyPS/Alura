666092028a897ac056984337cc7fee5897a00cf9
JessyPS <89113356+JessyPS@users.noreply.github.com>
2021-09-18 21:40:52 -0300

    Primeira aula Alura

---
+++ script.js
@@ -0,0 +1,15 @@
+var nome = "Jessica"
+var notaPrimeiroBimestre = 9
+var notaSegundoBimestre = 7
+var notaTerceiroBimestre = 4
+var notaQuartoBimestre = 8
+  
+var notaFinal = (notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimestre + notaQuartoBimestre) / 4
+
+var notaFixada = notaFinal.toFixed(1)
+
+console.log("Bem vindo " + nome)
+console.log(notaFixada)
+
+//Revisão:
+//Variáreis, string, console.log, toFixed, operações matemáticas, concatenação.
